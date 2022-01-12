5cab5e978285c948baf55545a8c9fe8ddc43046d
Mario Quiros Luna <marioquiros21@yahoo.com>
2022-01-11 20:56:13 -0600

    Fix: PhotoUser if url is undefined

--- src/shared/Components/PhotoUser/PhotoUser.jsx
+++ src/shared/Components/PhotoUser/PhotoUser.jsx
@@ -3,7 +3,11 @@ import './PhotoUser.scss'
 const PhotoUser = ({ url='https://www.computerhope.com/jargon/g/guest-user.jpg' }) => {
 	return (
 		<div className="photo__user-container">
-			<img src={url} alt="PhotoUser" width="48" />
+			{url ? 
+				<img src={url} alt="PhotoUser" width="48" />
+				:
+				<div>Photo</div>
+			}
 		</div>
 	)
 }
