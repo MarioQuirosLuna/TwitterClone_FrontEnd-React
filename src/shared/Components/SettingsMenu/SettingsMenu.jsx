370a249bfaf418d37aeeb7dd18be07d411841837
Mario Quiros Luna <marioquiros21@yahoo.com>
2022-01-11 20:42:11 -0600

    Add: Menu setting

---
+++ src/shared/Components/SettingsMenu/SettingsMenu.jsx
@@ -0,0 +1,15 @@
+import './SettingsMenu.scss'
+
+import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined'
+
+const SettingsMenu = () => {
+	return (
+		<div className="settingsMenu__container">
+			<div>
+				<MoreHorizOutlinedIcon />
+			</div>
+		</div>
+	)
+}
+
+export default SettingsMenu
