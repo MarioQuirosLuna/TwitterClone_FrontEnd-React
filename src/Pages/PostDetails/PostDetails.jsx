bce5b08ba3937b002d9d39905b9a3a63a63a0d55
Mario Quiros Luna <marioquiros21@yahoo.com>
2022-01-23 19:48:58 -0600

    Add: Show comments

--- src/Pages/PostDetails/PostDetails.jsx
+++ src/Pages/PostDetails/PostDetails.jsx
@@ -8,7 +8,14 @@ import PhotoUser from '../../shared/Components/PhotoUser/PhotoUser'
 import SettingsMenu from '../../shared/Components/SettingsMenu/SettingsMenu'
 import TextBlue from '../../shared/Components/TextBlue/TextBlue'
 
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined'
+import AutorenewOutlinedIcon from '@mui/icons-material/AutorenewOutlined'
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined'
+import IosShareOutlinedIcon from '@mui/icons-material/IosShareOutlined'
+
 import './PostDetails.scss'
+import BtnTwitter from '../../shared/Components/BtnTwitter/BtnTwitter'
+import TweetPost from '../../Components/TweetPost/TweetPost'
 
 const PostDetails = () => {
 
@@ -19,34 +26,62 @@ const PostDetails = () => {
 	useEffect(() => {
 		setPost(appContext?.posts?.find((post) => post.id === idPost))
 	}, [appContext?.posts, idPost])
-
+	console.log(post)
 	return (
 		<div>
-			<div className="tweet__container-photo">
-				<PhotoUser url={post?.user_photo} />
+			<div>
+				<div>
+					<PhotoUser url={post?.user_photo} />
+					<div>
+						<div>
+							<span>{post?.nameUser}</span>
+							<span>{post?.username}</span>
+						</div>
+						<div>
+							<SettingsMenu />
+						</div>
+					</div>
+				</div>
+				<div>
+					<div>
+						{post?.text_posted}
+					</div>
+					<div>
+						{post?.media_posted && <ImagePosted url={post?.media_posted} />}
+					</div>
+					<div>
+						<TextBlue label="Translate Tweet" />
+					</div>
+					<div>
+						<span>{useGetPostTime(post?.postTime, true)}</span>
+					</div>
+				</div>
+				<div>
+					<div>
+						<span>{post?.retweets?.length} Retweets</span>
+					</div>
+					<div>
+						<span>{post?.likes?.length} Likes</span>
+					</div>
+				</div>
+				<div>
+					<ChatBubbleOutlineOutlinedIcon />
+					<AutorenewOutlinedIcon />
+					<FavoriteBorderOutlinedIcon />
+					<IosShareOutlinedIcon />
+				</div>
+				<div>
+					<PhotoUser />
+					<input type="text" />
+					<div>
+						<BtnTwitter label="Reply" />
+					</div>
+				</div>
 			</div>
-			<div className="tweet__container-content">
-				<div className="content__nav">
-					<div className="content__nav-data">
-						<span className="nav__data-name">{post?.nameUser}</span>
-						<span className="nav__data-username">{post?.username}</span>
-					</div>
-					<div className="content__nav-settings">
-						<SettingsMenu />
-					</div>
-				</div>
-				<div className="content__text">
-					{post?.text_posted}
-				</div>
-				<div className="content__media">
-					{post?.media_posted && <ImagePosted url={post?.media_posted} />}
-				</div>
-				<div>
-					<TextBlue label="Translate Tweet" />
-				</div>
-				<div>
-					<span className="nav__data-time">{useGetPostTime(post?.postTime, true)}</span>
-				</div>
+			<div>
+				{post?.comments?.map((post, id) => {
+					return <TweetPost key={id} post={post} />
+				})}
 			</div>
 		</div>
 
