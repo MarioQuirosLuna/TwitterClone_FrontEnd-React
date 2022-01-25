455ab4517e7dbe3e5e70d50595a1b549c98fa8a3
Mario Quiros Luna <marioquiros21@yahoo.com>
2022-01-25 11:18:08 -0600

    Add: Partial SCSS PostDetails

---
+++ src/Components/NavPostDetails/NavPostDetails.jsx
@@ -0,0 +1,18 @@
+import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined'
+
+import './NavPostDetails.scss'
+
+const NavPostDetails = () => {
+	return (
+		<section className="header__navTweet">
+			<div>
+				<ArrowBackOutlinedIcon />
+			</div>
+			<div>
+				<h2>Tweet</h2>
+			</div>
+		</section>
+	)
+}
+
+export default NavPostDetails
