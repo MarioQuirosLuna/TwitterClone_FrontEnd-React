2203f077949ff624a3670dec5fda678bab41deb7
Mario Quiros Luna <marioquiros21@yahoo.com>
2022-01-29 12:14:01 -0600

    Fix: Photo user height

--- src/shared/Components/PhotoUser/PhotoUser.jsx
+++ src/shared/Components/PhotoUser/PhotoUser.jsx
@@ -1,10 +1,10 @@
 import './PhotoUser.scss'
 
-const PhotoUser = ({ url='https://www.computerhope.com/jargon/g/guest-user.jpg', size='48' }) => {
+const PhotoUser = ({ url = 'https://www.computerhope.com/jargon/g/guest-user.jpg', size = '48' }) => {
 	return (
 		<div className="photo__user-container">
-			{url ? 
-				<img src={url} alt="PhotoUser" width={size} />
+			{url ?
+				<img src={url} alt="PhotoUser" width={size} height={size} />
 				:
 				<div>Photo</div>
 			}
