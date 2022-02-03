16ce20a87e73b0a756ea610769b4e9746a1ea544
Mario Quiros Luna <marioquiros21@yahoo.com>
2022-02-03 13:21:53 -0600

    Fix: Key prop

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -13,12 +13,14 @@ const Menu = () => {
 	return (
 		<div className="menu__container">
 			<nav className="container__nav">
-				<label className="container__twittericon"><TwitterIcon fontSize='large'/></label>
+				<label className="container__twittericon"><TwitterIcon fontSize='large' /></label>
 				<MenuActiveProvider value={null}>
 					<ul>
 						{ListOptionsMenu?.map((option, index) => {
 							return (
-								<label className={option.label}><MenuItem key={index} option={option}/></label>
+								<label className={option.label} key={index}>
+									<MenuItem option={option} />
+								</label>
 							)
 						})}
 					</ul>
@@ -26,16 +28,16 @@ const Menu = () => {
 			</nav>
 			<div className="container__btnTweet">
 				<label type="button" className="btnTweet__tweet">Tweet</label>
-				<label type="button" className="btnTweet__icon"><CreateIcon/></label>
+				<label type="button" className="btnTweet__icon"><CreateIcon /></label>
 			</div>
 			<div className="container__profile">
-				<PhotoUser url={undefined} size="40"/>
+				<PhotoUser url={undefined} size="40" />
 				<div className="pofile_name-moreicon">
 					<div className="profile__name">
 						<label>User Name Logged</label>
 						<div><label className="profile__name-username">@username</label></div>
 					</div>
-					<div className="profile__moreicon"><MoreHorizOutlinedIcon/></div>
+					<div className="profile__moreicon"><MoreHorizOutlinedIcon /></div>
 				</div>
 			</div>
 		</div>
