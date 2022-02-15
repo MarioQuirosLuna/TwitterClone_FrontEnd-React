868438263ffa3ca83af6c4b2a2df0804d16d784b
Jorge <jorgemadrigalsolano81@gmail.com>
2022-02-15 10:34:55 -0600

    the principal list of tweets was added as an example and position fixed for bookmarks nav

--- src/Pages/Bookmarks/Bookmarks.js
+++ src/Pages/Bookmarks/Bookmarks.js
@@ -1,10 +1,30 @@
+import { useContext, useEffect } from 'react'
+
 import NavBookmarks from '../../Components/NavPages/NavBookmarks/NavBookmarks'
+import TweetPost from '../../Components/Tweet/TweetPost/TweetPost'
+import { AppContext } from '../../Context/AppContext'
+import { getAllPost } from '../../Services/api'
 import './Bookmarks.scss'
 
 const Message = () => {
+	const appContext = useContext(AppContext)
+
+	useEffect(() => {
+		const fetching = async () => {
+			appContext?.setPosts(await getAllPost())
+		}
+		fetching()
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
+
 	return (
-		<section className="message__container">
+		<section className="bookmarks__container">
 			<NavBookmarks />
+			<div className="bookmarks__tweetsList">
+				{appContext?.posts?.map((post, id) => {
+					return <TweetPost key={id} post={post} />
+				})}
+			</div>
 		</section>
 	)
 }
