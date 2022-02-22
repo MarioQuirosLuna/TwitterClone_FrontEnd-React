0fdd051f5293125aa323a4d39ae77602b0010a4b
Mario Quiros Luna <marioquiros21@yahoo.com>
2022-02-22 13:13:32 -0600

    Fix: Key on array maps

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -8,12 +8,12 @@ const Register = () => {
 	return (
 		<div className="register__container">
 			<div className="register__content">
-           		<div className="content__registerTop">
+				<div className="content__registerTop">
 					<div className="register__exit">
-						<label type="button" className="exit_button"><CloseIcon/></label>
+						<label type="button" className="exit_button"><CloseIcon /></label>
 					</div>
 					<div className="register__iconTwitter">
-						<TwitterIcon fontSize="large"/>
+						<TwitterIcon fontSize="large" />
 					</div>
 				</div>
 				<div className="content__registerLabel">
@@ -31,15 +31,15 @@ const Register = () => {
 				<div className="content__dateInfo">
 					<label className="Birthday">Birthday</label>
 					<div className="div__info">
-						<label className="info__label">This information will not be public. 
-						Confirm your own age, even if this account is for a business, 
-						a pet, or something else.</label>
+						<label className="info__label">This information will not be public.
+							Confirm your own age, even if this account is for a business,
+							a pet, or something else.</label>
 					</div>
 				</div>
 				<div className="content__birthdaySelect">
 					<div className="birthday__Month">
-						<select>
-							<option selected="selected" disabled> </option>
+						<select defaultValue>
+							<option> </option>
 							<option>January</option>
 							<option>February</option>
 							<option>March</option>
@@ -53,27 +53,27 @@ const Register = () => {
 							<option>November</option>
 							<option>December</option>
 						</select>
-						<label className="month__icon"><KeyboardArrowDownIcon fontSize="large"/></label>
+						<label className="month__icon"><KeyboardArrowDownIcon fontSize="large" /></label>
 						<label className="month__label">Month</label>
 					</div>
 					<div className="birthday__day">
-						<select>
-							<option selected="selected" disabled> </option>
+						<select defaultValue>
+							<option> </option>
 							{[...Array(31)].map((x, i) =>
-    							<option>{++i}</option>
-  							)}
+								<option key={`birthday__day_${i}`}>{++i}</option>
+							)}
 						</select>
-						<label className="day__icon"><KeyboardArrowDownIcon fontSize="large"/></label>
+						<label className="day__icon"><KeyboardArrowDownIcon fontSize="large" /></label>
 						<label className="day__label">Day</label>
 					</div>
 					<div className="birthday__year">
-						<select>
-							<option selected="selected" disabled> </option>
+						<select defaultValue>
+							<option> </option>
 							{[...Array(121)].map((x, i) =>
-    							<option>{2022-i}</option>
-  							)}
+								<option key={`birthday__year${i}`}>{2022 - i}</option>
+							)}
 						</select>
-						<label className="year__icon"><KeyboardArrowDownIcon fontSize="large"/></label>
+						<label className="year__icon"><KeyboardArrowDownIcon fontSize="large" /></label>
 						<label className="year__label">Year</label>
 					</div>
 				</div>
