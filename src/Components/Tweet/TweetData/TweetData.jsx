16a1285db6d0e3d817fa106927129ccd3f2bc0a9
Mario Quiros Luna <marioquiros21@yahoo.com>
2022-03-06 09:11:54 -0600

    Fix: Styles

--- src/Components/Tweet/TweetData/TweetData.jsx
+++ src/Components/Tweet/TweetData/TweetData.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import ImagePosted from '../../../shared/Components/ImagePosted/ImagePosted'
 import SettingsMenu from '../../../shared/Components/SettingsMenu/SettingsMenu'
 import PhotoUser from '../../../shared/Components/PhotoUser/PhotoUser'
@@ -6,6 +8,8 @@ import useGetPostTime from '../../../Hooks/useGetPostTime'
 
 const TweetData = ({
 	post: {
+		id,
+		_id,
 		user_photo,
 		nameUser,
 		username,
@@ -15,29 +19,33 @@ const TweetData = ({
 	}
 }) => {
 	return (
-		<>
-			<div className="tweet__container-photo">
-				<PhotoUser url={user_photo} />
-			</div>
-			<div className="tweet__container-content">
-				<div className="content__nav">
-					<div className="content__nav-data">
-						<span className="nav__data-name">{nameUser}</span>
-						<span className="nav__data-username">{username}</span>
-						<span className="nav__data-time">· {useGetPostTime(postTime)}</span>
+		<Link to={`/${username}/status/${id ? id : _id}`} className="link">
+			<div className="tweet__linkContainer">
+				<div className="tweet__container-tweetData">
+					<div className="tweet__container-photo">
+						<PhotoUser url={user_photo} />
 					</div>
-					<div className="content__nav-settings">
-						<SettingsMenu />
+					<div className="tweet__container-content">
+						<div className="content__nav">
+							<div className="content__nav-data">
+								<span className="nav__data-name">{nameUser}</span>
+								<span className="nav__data-username">{username}</span>
+								<span className="nav__data-time">· {useGetPostTime(postTime)}</span>
+							</div>
+						</div>
+						<div className="content__text">
+							{text_posted}
+						</div>
+						<div className="content__media">
+							{media_posted && <ImagePosted url={media_posted} />}
+						</div>
 					</div>
 				</div>
-				<div className="content__text">
-					{text_posted}
-				</div>
-				<div className="content__media">
-					{media_posted && <ImagePosted url={media_posted} />}
+				<div className="content__nav-settings">
+					<SettingsMenu />
 				</div>
 			</div>
-		</>
+		</Link>
 	)
 }
 
