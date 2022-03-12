3a8fe04c674f9cb132bbd30d16368114f0106393
Mario Quiros Luna <marioquiros21@yahoo.com>
2022-03-12 12:33:58 -0600

    Add: Styles and Layout for Default Page

--- src/Pages/Default/Default.js
+++ src/Pages/Default/Default.js
@@ -7,58 +7,84 @@ import './Default.scss'
 const Default = () => {
 	return (
 		<div className="default__containerPage">
-			<div className="default__menuInitial">
-				<div>
-					<div className="icon">
-						<i>
+			<div className="default__complete">
+				<div className="default__menuInitial">
+					<div className="content">
+						<div className="icon">
 							<TwitterIcon fontSize="large" />
-						</i>
-					</div>
-					<div className="text">
-						<p className="big">
-							What is happening now
-						</p>
-						<p className="little">
-							Join Twitter today.
-						</p>
-					</div>
-					<div className="signUpOptions">
-						<div className="option">
-							<i><GoogleIcon /></i>
-							<span>Sign up with Google</span>
 						</div>
-						<div className="option apple">
-							<i><AppleIcon /></i>
-							<span>Sign up with Apple</span>
+						<div className="text">
+							<span className="big">
+								What is happening now
+							</span>
+							<p className="little">
+								Join Twitter today.
+							</p>
 						</div>
-						<div className="sectionOr">
-							<div></div>
-							<div>
-								<span>or</span>
+						<div className="signUpOptions">
+							<div className="option">
+								<i><GoogleIcon /></i>
+								<span>Sign up with Google</span>
 							</div>
-							<div></div>
+							<div className="option apple">
+								<i><AppleIcon /></i>
+								<span>Sign up with Apple</span>
+							</div>
+							<div className="sectionOr">
+								<div></div>
+								<div>
+									<span>or</span>
+								</div>
+								<div></div>
+							</div>
+							<span className="option phoneEmail">
+								<span>Sign up with phone number or email</span>
+							</span>
+							<div className="message__cookies">
+								By signing up, you agree to the <span>Terms of Service</span> and <span>Privacy Policy</span>, including the <span>Use of Cookies policy</span>.
+							</div>
 						</div>
-						<span className="option phoneEmail">
-							Sign up with phone number or email
-						</span>
-						<div className="message__cookies">
-							By signing up, you agree to the Terms of Service and Privacy Policy, including the Use of Cookies policy.
+						<div className="logIn">
+							<div className="message__title">
+								Do you already have an account?
+							</div>
+							<div className="login__option">
+								Log in
+							</div>
 						</div>
 					</div>
-					<div className="logIn">
-						<div className="message__title">
-							Do you already have an account?
-						</div>
-						<div className="login__option">
-							Log in
-						</div>
+				</div>
+				<div className="default__imageInitial">
+					<div>
+						<img src="https://abs.twimg.com/sticky/illustrations/lohp_1302x955.png" alt="bg tweeter initial Page" />
+						<div></div>
+					</div>
+					<div className="logo">
+						<TwitterIcon fontSize="large" />
 					</div>
 				</div>
 			</div>
-			<div className="default__imageInitial">
-				<div>
-					<img src="https://abs.twimg.com/sticky/illustrations/lohp_1302x955.png" alt="bg tweeter initial Page" />
-				</div>
+			<div className="navigation">
+				<nav className="navBottom">
+					<span>About</span>
+					<span>Help center</span>
+					<span>Terms of Service</span>
+					<span>Privacy policies</span>
+					<span>Cookies policy</span>
+					<span>Accessibility</span>
+					<span>Ad information</span>
+					<span>Blog</span>
+					<span>State</span>
+					<span>Jobs</span>
+					<span>Brand resources</span>
+					<span>Advertising</span>
+					<span>Marketing</span>
+					<span>Twitter for business</span>
+					<span>Developers</span>
+					<span>Guide</span>
+					<span>Setting</span>
+					<span>&copy;{new Date().getFullYear()}</span>
+				</nav>
 			</div>
 		</div>
 	)
