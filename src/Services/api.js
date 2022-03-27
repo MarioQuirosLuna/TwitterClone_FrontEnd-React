c868b4db5919b7ea1bbf514d8f63b76fc5ad4336
Mario Quiros Luna <marioquiros21@yahoo.com>
2022-03-27 14:38:29 -0600

    Fix: Like with the different logged in users

--- src/Services/api.js
+++ src/Services/api.js
@@ -69,13 +69,13 @@ export const newComment = async (username, id, newComment) => {
 		.catch(error => { console.error(error) })
 }
 
-export const newLike = async (username, id, newLike) => {
+export const newLike = async (id, newLike) => {
 	const config = {
 		headers: {
 			'Content-type': 'application/json'
 		}
 	}
-	const req = axios.put(`${LOCAL_URL}/post/like/${username}/${id}`, newLike, config)
+	const req = axios.put(`${LOCAL_URL}/post/like/${id}`, newLike, config)
 	return req
 		.then((res) => res.data)
 		.catch(error => { console.error(error) })
