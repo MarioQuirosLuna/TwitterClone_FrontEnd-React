f734ee1386165345638cd2a72887ead6600c6bec
Mario Quiros Luna <marioquiros21@yahoo.com>
2022-04-12 09:20:32 -0600

    Fix: Photo User loading

--- src/shared/Components/PhotoUser/PhotoUser.jsx
+++ src/shared/Components/PhotoUser/PhotoUser.jsx
@@ -4,9 +4,9 @@ const PhotoUser = ({ url, size = '48' }) => {
 	return (
 		<div className="photo__user-container">
 			{(url && url !== '') ?
-				<img src={url} alt="PhotoUser" width={size} height={size} />
+				<img src={url} alt=" " width={size} height={size} loading='lazy' />
 				:
-				<img src={'https://www.computerhope.com/jargon/g/guest-user.jpg'} alt="PhotoUser" width={size} height={size} />
+				<img src={'https://www.computerhope.com/jargon/g/guest-user.jpg'} alt=" " width={size} height={size} loading='lazy' />
 			}
 		</div>
 	)
